248eb6b44fe14e4a079ccffe2b68b514716b417b
Kaustav B <76795611+CtrlShiftSammy@users.noreply.github.com>
2023-12-15 19:50:00 +0530

    init

---
+++ PNR_Ranking.py
@@ -0,0 +1,67 @@
+import pandas as pd
+
+PNR_Booking_csv = "Data/mphasis_dataset/PNRB-ZZ-20231208_062017.csv"
+PNR_Passenger_csv = "Data/mphasis_dataset/PNRP-ZZ-20231208_111136.csv"
+
+PNR_Booking_df = pd.read_csv(PNR_Booking_csv)
+print(PNR_Booking_df.head())
+PNR_Passenger_df = pd.read_csv(PNR_Passenger_csv)
+print(PNR_Passenger_df.head())
+
+#Identify impacted flights and passengers with the proposed schedule changes
+def returnImpactedPassengers(FLT_NUM):
+    # Filter PNR_Booking_df based on FLT_NUM
+    impacted_booking_df = PNR_Booking_df[PNR_Booking_df['FLT_NUM'] == FLT_NUM]
+
+    # Extract unique RECLOC values from the impacted booking DataFrame
+    impacted_reclocs = impacted_booking_df['RECLOC'].unique()
+
+    # Filter PNR_Passenger_df based on matching RECLOC values
+    impacted_passenger_df = PNR_Passenger_df[PNR_Passenger_df['RECLOC'].isin(impacted_reclocs)]
+
+    impacted_passenger_df = pd.merge(impacted_passenger_df, impacted_booking_df, on='RECLOC', how='left')
+    
+    # Add a new column DL_Conn = SEG_TOTAL - SEG_SEQ
+    impacted_passenger_df['DL_Conn'] = impacted_passenger_df['SEG_TOTAL'] - impacted_passenger_df['SEG_SEQ']
+
+    return impacted_passenger_df
+
+print(returnImpactedPassengers(2504).head())
+
+ImpactedPassengers = returnImpactedPassengers(2504)
+
+def load_rules_from_file(file_path):
+    # Implement logic to read rules from file (CSV, JSON, etc.)
+    # Return a DataFrame with rules
+    rules_df = pd.read_csv(file_path)  # Update this line based on your file format
+    return rules_df
+
+def rate_passengers(df, rules_df):
+    # Create a new column to store ratings
+    df['Passenger_Rating'] = 0
+
+    # Loop through each rule
+    for index, rule in rules_df.iterrows():
+        # Extract rule conditions and rating
+        conditions = rule['Conditions']
+        rating = rule['Rating']
+        var = rule['Variable']
+        # Evaluate conditions for each passenger
+        mask = df.eval(conditions)
+
+        # Assign rating to passengers meeting the conditions
+        df.loc[mask, 'Passenger_Rating'] += rating * ( 1 if (var == '.') else df[var])
+    return df
+
+# Example usage:
+# Assuming PNR_Passenger_df is the DataFrame containing PNR passenger information
+# Replace 'your_rules_file_path.csv' with the actual path to your rules file
+rules_file_path = 'Rules/rule_profile1/PNR_Rating.csv'
+rules_df = load_rules_from_file(rules_file_path)
+
+# Call the function to rate passengers
+rated_passengers_df = rate_passengers(ImpactedPassengers, rules_df)
+
+# Display the DataFrame with passenger ratings
+print(rated_passengers_df.head(100))
+
