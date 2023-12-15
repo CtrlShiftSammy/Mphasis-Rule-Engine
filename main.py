8cb80b23cd31f0bc168520238174fb5c85380070
Kaustav B <76795611+CtrlShiftSammy@users.noreply.github.com>
2023-12-16 01:50:15 +0530

    update

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-from Passenger_Ranking import RankPassengers
+import pandas as pd
 
-RankPassengers('ZZ20240406CCUHYD3723')
+from Passenger_Ranking import RankPassengers
+from Flight_Matching import MatchFlights
+from Flight_Matching import returnFlight
+from Flight_Ranking import RateFlights
+
+CancelledFlightDep_Key = 'ZZ20240406CCUHYD3723'
+#RankPassengers(CancelledFlightDep_Key)
+
+matched_flights_df = MatchFlights(CancelledFlightDep_Key)
+print(matched_flights_df)
+
+CancelledFlightINV = returnFlight(CancelledFlightDep_Key)
+
+# Add a new column 'Rating' to the matched_flights_df
+matched_flights_df['Rating'] = 0
+
+# Iterate through each row and fill the 'Rating' column
+for index, row in matched_flights_df.iterrows():
+    # Assuming RateFlights is a function that takes flight information and returns a rating
+    print(CancelledFlightINV)
+    print(row)
+    rating = RateFlights(CancelledFlightINV,row)  # Pass the flight information to the function
+    matched_flights_df.at[index, 'Rating'] = rating
+
+# Print the updated DataFrame with the 'Rating' column
+print(matched_flights_df[['InventoryId', 'FlightNumber', 'Dep_Key', 'DepartureAirport', 'ArrivalAirport', 'AircraftType','DepartureDate','DepartureDateTime','ArrivalDateTime']])
