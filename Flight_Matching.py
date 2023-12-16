b8bfdc3d64c8365aa04e6a05ff2567921bf2f5d4
Kaustav B <76795611+CtrlShiftSammy@users.noreply.github.com>
2023-12-16 19:56:04 +0530

    update

--- Flight_Matching.py
+++ Flight_Matching.py
@@ -5,6 +5,11 @@ FlightSchedule_csv = "Data/mphasis_dataset/SCH-ZZ-20231208_035117.csv"
 
 FlightInventory_df = pd.read_csv(FlightInventory_csv)
 FlightSchedule_df = pd.read_csv(FlightSchedule_csv)
+def load_rules_from_file(file_path):
+    # Implement logic to read rules from file (CSV, JSON, etc.)
+    # Return a DataFrame with rules
+    rules_df = pd.read_csv(file_path)  # Update this line based on your file format
+    return rules_df
 
 def returnFlight(DEP_KEY):
     matching_rows = FlightInventory_df[FlightInventory_df['Dep_Key'].astype(str) == DEP_KEY+".0"]
@@ -19,28 +24,20 @@ def returnFlight(DEP_KEY):
 
 def MatchFlights(DEP_KEY):
 # Filter FlightInventory_df for the given DEP_KEY
-#flight_inventory_row = FlightInventory_df[FlightInventory_df['Dep_Key'] == DEP_KEY]
     flight_inventory_row = FlightInventory_df[FlightInventory_df['Dep_Key'].astype(str).str.startswith(DEP_KEY)]
-
-    #print(FlightInventory_df.head())
+    
     # Get Departure Airport and Arrival Airport as single values
     departure_airport = flight_inventory_row['DepartureAirport'].iloc[0]
     arrival_airport = flight_inventory_row['ArrivalAirport'].iloc[0]
-    # print(flight_inventory_row)
-    # print("DEPARTURE", departure_airport)
-    # print("ARRIVAL", arrival_airport)
-    # print("ARRIVAL", FlightInventory_df['ArrivalAirport'])
-    # print("DEPARTURE", FlightInventory_df['DepartureAirport'])
     FlightSchedule_df['CombinedDateTime'] = pd.to_datetime(FlightSchedule_df['StartDate'] + ' ' + FlightSchedule_df['DepartureTime'])
-    # print(pd.to_datetime(flight_inventory_row['DepartureDateTime'].iloc[0]))
-    # print(FlightSchedule_df['CombinedDateTime'])
     matched_flights_df = FlightInventory_df[
         (FlightInventory_df['ArrivalAirport'] == arrival_airport) &
         (FlightInventory_df['DepartureAirport'] == departure_airport)
     ]
+
     rules_file_path = 'Rules/rule_profile1/Flight_Scoring.csv'
     rules_df = load_rules_from_file(rules_file_path)
     # print("Matched Flights:")
-    # print(matched_flights_df[['InventoryId', 'FlightNumber', 'DepartureAirport', 'ArrivalAirport', 'AircraftType','DepartureDate','DepartureDateTime','ArrivalDateTime']])
+    print(matched_flights_df[['InventoryId', 'FlightNumber', 'DepartureAirport', 'ArrivalAirport', 'AircraftType','DepartureDate','DepartureDateTime','ArrivalDateTime']])
     return(matched_flights_df)
 
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from Flight_Matching import MatchFlights
 from Flight_Matching import returnFlight
 from Flight_Ranking import rate_flights
 from Flight_Ranking import load_rules_from_file
+from Flight_selection import select_flight
+from Flight_selection import find_downline_connections
 
 CancelledFlightDep_Key = 'ZZ20240403BLRCCU2504'
 ranked_passengers_df = RankPassengers(CancelledFlightDep_Key)
@@ -20,12 +22,15 @@ matched_flights_df['Rating'] = 0
 # Iterate through each row and fill the 'Rating' column
 for index, row in matched_flights_df.iterrows():
     # Assuming RateFlights is a function that takes flight information and returns a rating
-    rules_df = load_rules_from_file('Rules/rule_profile1/Flight_Scoring.csv')
+    flight_scoring_rules_df = load_rules_from_file('Rules/rule_profile1/Flight_Scoring.csv')
+    flight_selection_rules_df = load_rules_from_file('Rules/rule_profile1/Flight_Selection.csv')
     row_df = pd.DataFrame([list(row)], columns=matched_flights_df.columns)
-    rating = rate_flights(CancelledFlightINV,row_df,rules_df)  # Pass the flight information to the function
-    matched_flights_df.loc[index, 'Rating'] = rating
+    rating = rate_flights(CancelledFlightINV,row_df,flight_scoring_rules_df)  # Pass the flight information to the function
+    if (select_flight(CancelledFlightINV, row_df, find_downline_connections(CancelledFlightDep_Key), flight_selection_rules_df)):
+        matched_flights_df.loc[index, 'Rating'] = rating
     #print(CancelledFlightINV)
     #print(row_df)
 
 # Print the updated DataFrame with the 'Rating' column
 print(matched_flights_df[['InventoryId', 'FlightNumber', 'Dep_Key', 'DepartureAirport', 'ArrivalAirport', 'AircraftType','DepartureDate','DepartureDateTime','ArrivalDateTime', 'Rating']])
+
